8e794f120b6a06f8b34937b8e2f167fa351d90d7
HaseebHassan <mhhassan095@gmail.com>
2021-10-07 17:56:18 -0700

    express ko gaddi

--- server/index.js
+++ server/index.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const app = express();
 const http = require('http');
-
+app.use(express.static(__dirname + '/public/assets/css'));
+app.use(express.static(__dirname + '/public/assets/js'));
 
 const server = app.listen(3000, () => {
     console.log('listening on *:3000');
